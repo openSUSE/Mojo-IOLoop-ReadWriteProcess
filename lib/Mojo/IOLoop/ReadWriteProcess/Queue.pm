b1fa9a3dfe78aeb4a10551cd71e87c81b0065edf
Ettore Di Giacinto <mudler@gentoo.org>
2017-09-24 13:28:24 +0200

    Update POD and format

--- lib/Mojo/IOLoop/ReadWriteProcess/Queue.pm
+++ lib/Mojo/IOLoop/ReadWriteProcess/Queue.pm
@@ -1,8 +1,8 @@
 package Mojo::IOLoop::ReadWriteProcess::Queue;
 use Mojo::Base -base;
-use Scalar::Util qw(blessed);
 use Mojo::IOLoop::ReadWriteProcess::Pool;
 use Mojo::IOLoop::ReadWriteProcess;
+
 has queue => sub { Mojo::IOLoop::ReadWriteProcess::Pool->new() };
 has pool  => sub { Mojo::IOLoop::ReadWriteProcess::Pool->new() };
 
@@ -19,9 +19,7 @@ sub _dequeue {
   $self->pool->last->start if $self->auto_start;
 }
 
-sub exhausted {
-  shift->pool->size == 0;
-}
+sub exhausted { shift->pool->size == 0 }
 
 sub consume {
   my $p = shift;
@@ -65,69 +63,69 @@ Mojo::IOLoop::ReadWriteProcess::Queue - Queue for Mojo::IOLoop::ReadWriteProcess
 
 =head1 SYNOPSIS
 
-    use Mojo::IOLoop::ReadWriteProcess qw(parallel);
+    use Mojo::IOLoop::ReadWriteProcess qw(queue process);
     my $n_proc = 20;
     my $fired;
 
-    my $p = parallel sub { print "Hello world\n"; } => $n_proc;
+    my $q = queue auto_start => 1;
+
+    $q->pool->maximum_processes(2); # Max 2 processes in parallel
+    $q->queue->maximum_processes(10); # Max queue is 10
+
+    $q->add( process sub { return 42 } ) for 1..7;
 
     # Subscribe to all "stop" events in the pool
-    $p->once(stop => sub { $fired++; });
+    $q->once(stop => sub { $fired++; });
 
-    # Start all processes belonging to the pool
-    $p->start();
-
-    # Receive the process output
-    $p->each(sub { my $p = shift; $p->getline(); });
-    $p->wait_stop;
-
-    # Get the last one! (it's a Mojo::Collection!)
-    $p->last()->stop();
+    # Consume the queue
+    $q->consume();
 
 =head1 METHODS
 
-L<Mojo::IOLoop::ReadWriteProcess::Pool> inherits all methods from L<Mojo::Collection> and implements
+L<Mojo::IOLoop::ReadWriteProcess::Queue> inherits all methods from L<Mojo::Base> and implements
 the following new ones.
 Note: It proxies all the other methods of L<Mojo::IOLoop::ReadWriteProcess> for the whole process group.
 
-=head2 get
-
-    use Mojo::IOLoop::ReadWriteProcess qw(parallel);
-    my $pool = parallel(sub { print "Hello" } => 5);
-    $pool->get(4);
-
-Get the element specified in the pool (starting from 0).
-
 =head2 add
 
-    use Mojo::IOLoop::ReadWriteProcess qw(pool);
-    my $pool = pool(maximum_processes => 2);
-    $pool->add(sub { print "Hello 2! " });
+    use Mojo::IOLoop::ReadWriteProcess qw(queue process);
+    my $q = queue();
+    $q->add(sub { print "Hello 2! " });
+    $q->add(process sub { print "Hello 2! " });
 
-Add the element specified in the pool.
+Add the process to the queue.
 
-=head2 remove
+=head2 consume
 
-    use Mojo::IOLoop::ReadWriteProcess qw(parallel);
-    my $pool = parallel(sub { print "Hello" } => 5);
-    $pool->remove(4);
+    use Mojo::IOLoop::ReadWriteProcess qw(queue);
+    my $q = queue();
+    $q->add(sub { print "Hello 2! " });
+    $q->add(process sub { print "Hello 2! " });
+    $q->consume; # executes and exhaust the processes
 
-Remove the element specified in the pool.
+Starts the processes and empties the queue.
+Note: maximum_processes can be set both to the pool (number of process to be run in parallel),
+and for the queue (that gets exhausted during the C<consume()> phase).
 
-=head2 maximum_processes
+    $q->pool->maximum_processes(2); # Max 2 processes in parallel
+    $q->queue->maximum_processes(10); # Max queue is 10
 
-    use Mojo::IOLoop::ReadWriteProcess qw(parallel);
-    my $pool = parallel(sub { print "Hello" } => 5);
-    $pool->maximum_processes(30);
-    $pool->add(...);
+=head2 exhausted
 
-Prevent from adding processes to the pool. If we reach C<maximum_processes> number
-of processes, C<add()> will refuse to add more to the pool.
+    use Mojo::IOLoop::ReadWriteProcess qw(queue);
+    my $q = queue();
+    $q->add(sub { print "Hello 2! " });
+    $q->add(process sub { print "Hello 2! " });
+    $q->consume; # executes and exhaust the processes
+    $q->exhausted; # 1
+
+Returns 1 if the queue is exhausted.
 
 =head1 ENVIRONMENT
 
 You can set the MOJO_PROCESS_MAXIMUM_PROCESSES environment variable to specify the
-the maximum number of processes allowed in L<Mojo::IOLoop::ReadWriteProcess> instances.
+the maximum number of processes allowed in the pool and the queue, that are
+L<Mojo::IOLoop::ReadWriteProcess::Pool> instances.
 
     MOJO_PROCESS_MAXIMUM_PROCESSES=10000
 
